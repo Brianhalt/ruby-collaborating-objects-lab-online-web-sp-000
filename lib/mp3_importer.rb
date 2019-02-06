c8b6351a8b1bc279ad3879e44b6ae453b5948275
Brian Haltiwanger <bhalt@bellsouth.net>
2019-02-06 00:40:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,11 +9,11 @@ class MP3Importer
 
   def files
     @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
-    binding.pry
   end
 
   def import
     files.each {|song| Song.new_by_filename(song)}
+    binding.pry
   end
 
 end
